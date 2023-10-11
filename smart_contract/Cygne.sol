05e78f559c2a9beeda846d16f7c63bef1fa91778
nono <arnaudjourdain111@gmail.com>
2023-10-11 12:41:23 +0200

    Token Interface

--- smart_contract/Cygne.sol
+++ smart_contract/Cygne.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
+
+import "./Token.sol";
 
 contract Cygne {
-    constructor(){
 
-    }
+    mapping (address => Token) teacherToERC;
+
+
 }
