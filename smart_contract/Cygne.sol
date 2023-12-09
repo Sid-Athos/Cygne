21290b2b7b7a9eeeaa5ea4aa568bafa868f515af
nono <arnaudjourdain111@gmail.com>
2023-12-09 16:37:29 +0100

    updated

--- smart_contract/Cygne.sol
+++ smart_contract/Cygne.sol
@@ -1,57 +1,89 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-import "./Teacher.sol";
-import "./Token.sol";
-import "./TokenInstant.sol";
+pragma solidity ^0.4.19;
 
 contract Cygne {
 
     struct Teacher {
         string name;
-        address token;
-        Course[] courses;
+        uint256[] coursesId;
     }
 
     struct Course {
         string datetime;
         string location;
         uint256 price;
+        address[] subscribersAddress;
+        uint256 sizeSubscribers;
+        bool isPaid;
+        address teacherAddress;
     }
 
-    mapping (address => Teacher) private addressToTeacher;
-    uint tokenValue = 0.01 ether;
+    Course[] public courses;
+    Teacher[] public teacherList;
+    uint256 sizeCourses = 0;
 
-    function registerAsTeacher(string name, string tokenName, string symbol) public {
-        require(!addressToTeacher[msg.sender].isValue);
-        addressToTeacher[msg.sender] = Teacher(
-            name,
-            TokenInstance(msg.sender, tokenName, symbol),
-            Course[]
-        );
+    mapping(address => uint256) public addressToTeacherId;
+//uint tokenValue = 0.01 ether;
+
+    function registerAsTeacher(string memory _name) public view returns (uint256 teacherId) {
+        uint256 id = teacherList.push(Teacher(_name, new uint[](0))) - 1;
+        addressToTeacherId[msg.sender] = id;
+        return id;
     }
 
-    function createCourse(string datetime, string location, uint256 price) public {
-        require(addressToTeacher[msg.sender].isValue);
-        addressToTeacher[msg.sender].courses.push(Course(datetime, location, price));
+    function createCourse(string memory datetime, string memory location, uint256 price) public view returns (uint256 courseId) {
+        uint256 id = courses.push(Course(datetime, location, price, new address[](0), 0, false, msg.sender)) - 1;
+        teacherList[addressToTeacherId[msg.sender]].coursesId.push(id);
+        return id;
     }
 
-    function getTeacher(address teacherAddress) public view returns (Teacher teacher) {
-        return addressToTeacher[teacherAddress];
+    function getCourseList(address teacherAddress) public view returns (Course[] memory courseList) {
+        return courses;
     }
 
-    function mintToken(uint256 amount, address teacherAddress) public payable {
-        require(amount * tokenValue == msg.value);
-        addressToTeacher[teacherAddress].token.mint(msg.sender, amount);
+    function getTeacherList(address teacherAddress) public view returns (Teacher[] memory teachers) {
+        return teacherList;
     }
 
-    function burnToken(uint256 amount) public {
-        require(addressToTeacher[msg.sender].isValue);
-        addressToTeacher[msg.sender].token.burn(amount);
-        msg.sender.send(tokenValue * amount);
+    function getTeacher(address teacherAddress) public view returns (Teacher memory teacher) {
+        return teacherList[addressToTeacherId[teacherAddress]];
     }
 
-    function participateCourse(address teacherAddress, uint256 courseId) public {
-        IERC20(addressToTeacher[teacherAddress].token).transferFrom(msg.sender, teacherAddress, addressToTeacher[teacherAddress].courses[courseId].price);
+    function getCoursesByTeacher(address teacherAddress) public view returns (Course[] memory coursesReturned) {
+        Course[] memory coursesFromTeacher;
+        uint256 size = 0;
+
+        for (uint256 i = 0; i < sizeCourses; i++) {
+            if (courses[i].teacherAddress == teacherAddress) {
+                size++;
+            }
+        }
+
+        coursesFromTeacher = new Course[](size);
+        size = 0;
+
+        for (i = 0; i < sizeCourses; i++) {
+            if (courses[i].teacherAddress == teacherAddress) {
+                coursesFromTeacher[size] = courses[i];
+                size++;
+            }
+        }
+
+        return coursesFromTeacher;
+    }
+
+    function getCourseById(uint256 courseId) public view returns (Course memory course) {
+        return courses[courseId];
+    }
+
+    function payCourse(uint256 courseId) public payable {
+        require(msg.value == courses[courseId].price && msg.sender != courses[courseId].teacherAddress);
+        courses[courseId].sizeSubscribers++;
+        courses[courseId].subscribersAddress.push(msg.sender);
+    }
+
+    function payTeacher(uint256 courseId) public {
+        require(msg.sender == courses[courseId].teacherAddress);
+        courses[courseId].isPaid = true;
+        msg.sender.send(courses[courseId].price * courses[courseId].sizeSubscribers);
     }
 }
